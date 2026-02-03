abf3ffc4b4eedc2aa37f60f4b6363c2d17b41730
Sarah Kriborg <sarah.kriborg@gmail.com>
2026-02-03 11:00:29 +0100

    Return 100.0

--- itempricecalculator/itempricecalculator.go
+++ itempricecalculator/itempricecalculator.go
@@ -1,19 +1,26 @@
 package itempricecalculator
 
 // Implement an item price calculator that computes the total price of a list of items based on their type and quantity.
-//TRUCK items costs 100.0 each and PARCEL items costs 25.0 each.
+// TRUCK items costs 100.0 each and PARCEL items costs 25.0 each.
 //
-//If TRUCK items weigh more than 100kg, they cost an additional 50.0 per piece.
-//If PARCEL items weigh more than 10kg, they cost an additional 25.0 per piece.
+// If TRUCK items weigh more than 100kg, they cost an additional 50.0 per piece.
+// If PARCEL items weigh more than 10kg, they cost an additional 25.0 per piece.
 //
-//Items without weight information should be assumed to be outside the weight limit.
+// Items without weight information should be assumed to be outside the weight limit.
 //
-//Non-existing item types should return an error.
+// Non-existing item types should return an error.
+type Item struct {
+	Type     string
+	Quantity int
+}
+
+type ItemPriceCalculator struct {
+}
 
 func NewItemPriceCalculator() {
 
 }
 
-func CalculatePrice() {
-
+func (i *ItemPriceCalculator) CalculatePrice() float64 {
+	return 100.0
 }
--- itempricecalculator/itempricecalculator_test.go
+++ itempricecalculator/itempricecalculator_test.go
@@ -1,5 +1,12 @@
 package itempricecalculator_test
 
+import (
+	"testing"
+
+	"github.com/sarahk189/tddplayground/itempricecalculator"
+	"github.com/stretchr/testify/assert"
+)
+
 /*
 Implement an item price calculator that computes the total price of a list of items based on their type and quantity.
 TRUCK items costs 100.0 each and PARCEL items costs 25.0 each.
@@ -11,3 +18,16 @@ Items without weight information should be assumed to be outside the weight limi
 
 Non-existing item types should return an error.
 */
+
+func Test_TruckItemShouldCost100(t *testing.T) {
+	t.Parallel()
+
+	//ARRANGE
+	itemPriceCalculator := itempricecalculator.NewItemPriceCalculator()
+
+	//ACT
+	price := itemPriceCalculator.CalculatePrice()
+
+	//ASSERT
+	assert.Equal(t, 100.0, price)
+}
